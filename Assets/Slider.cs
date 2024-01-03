3b01e4698edffbe14e81f2501890fb3d9fee51bc
iiiinoint <nointiiii@gmail.com>
2024-01-03 15:18:12 +0800

    合并UI到main

---
+++ Assets/Slider.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+public class RageBarScript : MonoBehaviour
+{
+    public BossScript boss; // 引用Boss脚本
+    private Slider rageSlider; // 引用UI滑动条
+
+    private void Start()
+    {
+        rageSlider = GetComponent<Slider>();
+        if (boss != null)
+        {
+            UpdateRageBar(boss.GetCurrentRagePercent()); // 初始化怒气条
+            boss.onRageChanged += UpdateRageBar; // 订阅怒气值变化事件
+        }
+    }
+
+    // 更新进度条的值
+    private void UpdateRageBar(float value)
+    {
+        rageSlider.value = value;
+    }
+}
